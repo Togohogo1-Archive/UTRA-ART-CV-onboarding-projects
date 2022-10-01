86f72274a1ca94837937f1036d3d3c68c00d1864
Kevin Wang <59260907+Togohogo1@users.noreply.github.com>
2022-10-01 15:32:55 -0400

    some open cv stuff

---
+++ resize_image.py
@@ -0,0 +1,23 @@
+import cv2
+
+# factor = int(input("Shrink image by what factor: "))
+factor = 0.5
+
+# Read image
+img = cv2.imread("mountain.jpeg")
+shape = img.shape
+
+width = shape[1]
+height = shape[0]
+
+new_width = int(width*factor)
+new_height = int(height*factor)
+
+# output = cv2.resize(img, (new_width, new_height) interpolation=cv2.INTER_CUBIC if factor > 1 else cv2.INTER_AREA)
+output = cv2.resize(img, (new_width, new_height))
+
+
+
+print(width, height)
+print(img.shape)
+print(type(img))
