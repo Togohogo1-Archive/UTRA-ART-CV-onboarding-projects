402ba950f62447f3f54ab0b163ba5e6969fc84b2
Kevin Wang <59260907+Togohogo1@users.noreply.github.com>
2022-10-03 19:41:00 -0400

    ii) without color spaces

---
+++ split_channels.py
@@ -0,0 +1,13 @@
+import cv2
+
+img = cv2.imread("mountain.jpeg")
+
+# Split image into its respective channels
+B, G, R = cv2.split(img)
+
+cv2.imshow("BLUE", B)
+cv2.imshow("GREEN", G)
+cv2.imshow("RED", R)
+
+# Waits for the user to press any key
+cv2.waitKey(0)
