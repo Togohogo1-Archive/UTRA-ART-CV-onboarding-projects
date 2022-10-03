e0083cbaf60508ecbf4332f28951d9d98a27d0ca
Kevin Wang <59260907+Togohogo1@users.noreply.github.com>
2022-10-03 19:26:26 -0400

    finished question 1

--- resize_image.py
+++ resize_image.py
@@ -1,11 +1,10 @@
 import cv2
 
-# factor = int(input("Shrink image by what factor: "))
 factor = 2
 
 # Read image
 img = cv2.imread("mountain.jpeg")
-shape = img.shape
+shape = img.shape  # numpy array
 
 width = shape[1]
 height = shape[0]
@@ -13,13 +12,6 @@ height = shape[0]
 new_width = int(width*factor)
 new_height = int(height*factor)
 
-# output = cv2.resize(img, (new_width, new_height) interpolation=cv2.INTER_CUBIC if factor > 1 else cv2.INTER_AREA)
-output = cv2.resize(img, (new_width, new_height))
+output = cv2.resize(img, (new_width, new_height), interpolation=cv2.INTER_CUBIC if factor > 1 else cv2.INTER_AREA)
 
-
-
-print(width, height)
-print(img.shape)
-print(type(img))
-
-cv2.imwrite("mountain copy.jpeg",  output)
+cv2.imwrite("output.jpeg",  output)
