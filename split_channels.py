ac7bb9a2c793de4dffbf34162cd5cdda9934ef14
Kevin Wang <59260907+Togohogo1@users.noreply.github.com>
2022-10-04 11:41:52 -0400

    try out hsv

--- split_channels.py
+++ split_channels.py
@@ -1,13 +1,18 @@
 import cv2
 
 img = cv2.imread("rgb.png")
+hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+print(cv2.split(img))
 
 # Split image into its respective channels
-B, G, R = cv2.split(img)
+H, S, V = cv2.split(hsv_img)
 
-cv2.imshow("BLUE", B)
-cv2.imshow("GREEN", G)
-cv2.imshow("RED", R)
+print(cv2.split(hsv_img))
+
+cv2.imshow("BLUE", H)
+cv2.imshow("GREEN", S)
+cv2.imshow("RED", V)
 
 # Waits for the user to press any key
 cv2.waitKey(0)
