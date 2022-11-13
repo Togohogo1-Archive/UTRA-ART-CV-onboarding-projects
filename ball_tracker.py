53e1aa63825607198eb8cef4beed45f5677b82cc
Kevin Wang <59260907+Togohogo1@users.noreply.github.com>
2022-11-12 22:20:15 -0500

    Update ball_tracker.py

--- ball_tracker.py
+++ ball_tracker.py
@@ -12,19 +12,26 @@ while True:
     if isTrue:
         # cv.imshow("Video", frame)
 
-        time.sleep(0.1)
+        time.sleep(0.05)
 
         blank = np.zeros(frame.shape, dtype="uint8")
-        frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-        _, thresh = cv.threshold(frame[:, 0:250], 175, 255, cv.THRESH_BINARY)
+        bw_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        _, thresh = cv.threshold(bw_frame[:, 0:250], 175, 255, cv.THRESH_BINARY)
         thresh = cv.GaussianBlur(thresh, (5, 5), 1)
+        thresh = cv.dilate(thresh, (5, 5), 1)
         contours, _ = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
         if contours:
             print("drew")
-            cv.drawContours(blank, contours, -1, ( 255), thickness=1)
+            cv.drawContours(blank, contours, 0, ( 255), thickness=1)
+            x, y, w, h = cv.boundingRect(contours[0])  # Bounding rectangle for largest contour area (sun)
+        else:
+            break
 
-        cv.imshow("thrash", blank)
+        box_frame = frame.copy()
+        cv.rectangle(box_frame, (x, y), (x+w, y+h), (0, 255, 0), 1)
+
+        cv.imshow("thrash", box_frame)
 
         if cv.waitKey(20) & 0xFF == ord("d"):
             break
